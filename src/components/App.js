1a2d2c6172419661c36123f89eda199d3710aad3
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-13 16:22:28 -0700

    added props to my components

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,11 @@ import Header from './Header';
 export default function App() {
   return (
     <>
-      <Header />
+      <Header name="DongButt" />
       <h1>Hello World</h1>
-      <Cat />
+      <Cat name="Morris" age="unknown but maybe 16" weight="26lbs" />
+      <Cat name="Butter" age="9" weight="14lbs" />
+      <Cat name="Paddington" age="4 or 5" weight="16lbs" />
     </>
   );
 
