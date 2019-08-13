582ca6c91dd23c0d905db01f3aa96e105affffa4
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-13 15:25:01 -0700

    updated up to contain header and cat contents

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Cat from './Cat';
+import Header from './Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+      <Cat />
+    </>
+  );
+
 }
   
