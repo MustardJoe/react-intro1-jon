e708176c5b6df200265acee82e84314ae74ac62e
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-14 11:32:35 -0700

    added a lot here but still not working

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const styles = {
-  backgroundColor: hex,
-  with: '0.8em',
+function Color({ name, hex }) {
+  const color = {
+    name,
+    hex
+  };
+  const fields = Object.keys(color)
+    .map(key => {
+      return (
+        <>
+          <dt>{key}</dt>
+          <dd style={{ color: hex }}></dd>
+        </>
+      );
+    });
+
+
+  return (
+    <dl>
+      {fields}
+    </dl>
+  );
 }
 
-return (
-  
-)
+Color.propTypes = {
+  name: PropTypes.string.isRequired,
+  hex: PropTypes.string.isRequired,
+};
+
+export default Color;
