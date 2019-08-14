c5558fb98035f3d3666fff38dc9d72b6ada71fc6
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-14 11:30:13 -0700

    changed a bunch of stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Cat from './Cat';
 import Header from './Header';
+import ColorPicker from './ColorPicker';
 
 export default function App() {
   return (
     <>
-      <Header name="DongButt" />
+      <Header name="Rad Guy" />
       <h1>Hello World</h1>
       <Cat name="Morris" age="unknown but maybe 16" weight="26lbs" />
+      <ColorPicker />
       <Cat name="Butter" age="9" weight="14lbs" />
       <Cat name="Paddington" age="4 or 5" weight="16lbs" />
+
     </>
   );
 
