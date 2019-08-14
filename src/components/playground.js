0ee51db0058fdac3247b54f5c069a409a7d9bd5c
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-14 14:55:25 -0700

    added playground file

---
+++ src/components/playground.js
@@ -0,0 +1,8 @@
+const catsIKnow = ['Big Mor', 'Butter', 'Paddington Bear', 
+  'Tanookie', 'Norman', 'Brer'];
+
+for(let cat of catsIKnow) {
+  console.log(cat);
+  if(cat === 'Norman') { break; }
+}
+
