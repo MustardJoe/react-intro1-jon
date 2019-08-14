7fc8192627637df87f4a4565905c21df757962d4
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-14 11:32:54 -0700

    maybe i will have time to work on this

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// export default function header({ name }) {
+//   const headerMsg = 'Dont Come to the Party Today, ';
+
+//   return (
+//     <h1>{headerMsg} {name}</h1>
+//   );
+
+class Footer extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Footer;
