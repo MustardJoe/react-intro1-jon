700a58dae9aa20b4fa2768f2b6e762bcc6704134
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-15 11:37:48 -0700

    added my first state thing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import Cat from './Cat';
 import Header from './Header';
 import ColorPicker from './ColorPicker';
 import Footer from './Footer';
 import Button from './Button';
+import State from './State';
 
 export default function App() {
   return (
     <>
       <Header name="Rad Guy" />
+      <State />
       <h1>Hello World</h1>
       <Cat name="Morris" age="unknown but maybe 16" weight="26lbs" />
       <ColorPicker />
