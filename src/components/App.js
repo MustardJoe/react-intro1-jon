186defd7198d47a7add065e511240f431c082d07
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-15 23:50:11 -0700

    added colors for color list

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import ColorPicker from './ColorPicker';
 import Footer from './Footer';
 import Button from './Button';
 import State from './State';
+import Colors from './Colors';
+
+const colors = [
+  { name: 'red', rgb: { red: 255, green: 0, blue: 0 } },
+  { name: 'green', rgb: { red: 0, green: 255, blue: 0 } },
+  { name: 'orange', rgb: { red: 255, green: 165, blue: 0 } },
+  { name: 'hotpink', rgb: { red: 255, green: 105, blue: 180 } }
+];
 
 export default function App() {
   return (
@@ -14,6 +22,7 @@ export default function App() {
       <State />
       <h1>Hello World</h1>
       <Cat name="Morris" age="unknown but maybe 16" weight="26lbs" />
+      <Colors colors={colors} />
       <ColorPicker />
       <Cat name="Butter" age="9" weight="14lbs" />
       <Button buttonProp="good for you" />
