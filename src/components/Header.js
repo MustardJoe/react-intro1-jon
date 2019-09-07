344802afd5b6f080adf815ba6e06ca849adaca20
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-06 22:12:37 -0700

    delted old playground files to reuse this repo for fun

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function header({ name }) {
-  const headerMsg = 'Dont Come to the Party Today, ';
-
-  return (
-    <h1>{headerMsg} {name}</h1>
-  );
-}
--- src/components/playground.js
+++
@@ -1,8 +0,0 @@
-const catsIKnow = ['Big Mor', 'Butter', 'Paddington Bear', 
-  'Tanookie', 'Norman', 'Brer'];
-
-for(let cat of catsIKnow) {
-  console.log(cat);
-  if(cat === 'Norman') { break; }
-}
-
