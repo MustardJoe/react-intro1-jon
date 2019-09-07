619afb9986d0f026cd02bd58c75bc270a20daa3c
Jonathan Forney <jonathan.forney@gmail.com>
2019-09-06 23:52:47 -0700

    imported component to app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import WaterTracker from '../containers/WaterTracker';
 
 export default function App() {
   return (
     <>
    
       <h1>Hello World</h1>
-   
+      <WaterTracker />
     </>
   );
 }
